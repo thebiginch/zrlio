94d1c6d5d701059f48bb0b45e0cfda54fbaf32e0
Zachary Levine <zrlevine@gmail.com>
2017-05-07 19:52:25 -0400

    add server

---
+++ server/app.js
@@ -0,0 +1,26 @@
+var path = require("path");
+var bodyParser = require("body-parser");
+var morgan = require("morgan");
+var express = require("express");
+var app = express();
+var build = path.join(__dirname, "../build");
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("tiny"));
+
+app.use(express.static(`${build}`));
+
+app.get("/", function(req, res) {
+  res.sendFile(`${build}/index.html`);
+});
+
+app.post("/mail", function(req, res) {
+  console.log(req.body.contactName);
+
+  res.send("got it");
+});
+
+app.listen(2345, function() {
+  console.log("listening on port 2345");
+});
