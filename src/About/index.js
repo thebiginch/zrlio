4a7987187bf927b7cd2e9109d3bdc903c62314f1
Zachary Levine <zrlevine@gmail.com>
2017-05-12 23:05:38 -0400

    style about section

--- src/About/index.js
+++ src/About/index.js
@@ -1,21 +1,20 @@
 import React, { Component } from "react";
 import "./About.css";
+import bio from "./bio.js";
 
 export default class About extends Component {
   render() {
     return (
-      <section className="about">
-
-        <img className="picture" src="zrl.png" />
-        <div className="blurb">
-          <h2> About me</h2>
-          <hr />
-          <p>
-            Hi. My name is Zachary Levine.  I'm a full stack web developer.  I love programming because it challenges the mind to think and solve problems in a logical way.
-            I also like riding my bike, learning new things, and dogs.
-          </p>
+      <section className="about container">
+        <div className="row">
+          <div className="picture col-xs-12 col-sm-12 col-md-4">
+            <img className="center-block" src="zrl.png" />
+          </div>
+          <div className="blurb col-xs-12 col-sm-12 col-md-offset-1 col-md-6">
+            <h2> About me </h2> <hr />
+            <p> {bio} </p>
+          </div>
         </div>
-
       </section>
     );
   }
