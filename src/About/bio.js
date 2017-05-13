8218bb6044f83f9137fea85e7b68c0b4645e1b73
Zachary Levine <zrlevine@gmail.com>
2017-05-12 23:04:15 -0400

    style about section

---
+++ src/About/bio.js
@@ -0,0 +1,2 @@
+module.exports =
+  "Hi. My name is Zachary Levine. I'm a full stack web developer and engineer. I love programming because it challenges the mind to think and solve problems in a logical way. I also like riding my bike, learning new things, and dogs.";
