facb0090b6b78f0a84d7f3ced20def11f4bfbcd0
Zachary Levine <zrlevine@gmail.com>
2017-05-16 15:07:42 -0400

    change bio text and add link

--- src/About/bio.js
+++ src/About/bio.js
@@ -1,2 +1,4 @@
-module.exports =
-  "Hi. My name is Zachary Levine. I'm a full stack web developer and engineer. I love programming because it challenges the mind to think and solve problems in a logical way. I also like riding my bike, learning new things, and dogs.";
+module.exports = {
+  line1: "Hi. My name is Zachary Levine. I'm a full stack web developer and engineer. I love programming because it challenges the mind to think and solve problems in a logical way. I also like riding my bike, learning new things, and dogs.",
+  line2: "Check out what I've been working on below"
+};
--- src/About/index.js
+++ src/About/index.js
@@ -7,12 +7,13 @@ export default class About extends Component {
     return (
       <section className="about container">
         <div className="row">
-          <div className="picture col-xs-12 col-sm-12 col-md-4">
+          <div className="picture col-xs-12 col-sm-12 col-md-offset-1 col-md-4">
             <img className="center-block" src="zrl.png" />
           </div>
           <div className="blurb col-xs-12 col-sm-12 col-md-offset-1 col-md-6">
             <h2> About me </h2> <hr />
-            <p> {bio} </p>
+            <p> {bio.line1} </p>
+            <p><a href="#tech">{bio.line2}&nbsp;:)</a></p>
           </div>
         </div>
       </section>
